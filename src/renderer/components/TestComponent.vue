7d02af295caa05b592b5bfb3fbbd9f9acf7846c3
shogo-yamato <shogo.yamato@arsaga.jp>
2021-08-19 16:30:28 +0900

    feat(package.json): installed and enabled stylelint packages for rscss

---
+++ src/renderer/components/TestComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="test-component">
+    <h1 class="title">This is a test component.</h1>
+    <nuxt-link to="/tutorial">INTERNAL LINK</nuxt-link>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped>
+@import 'assets/scss/app';
+
+.test-component {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  > .title {
+    font-size: 5rem;
+    line-height: 1.5;
+  }
+}
+</style>
--- src/renderer/pages/index.vue
+++ src/renderer/pages/index.vue
@@ -1,9 +1,25 @@
 <template>
-  <Tutorial />
+  <div class="index-container">
+    <test-component />
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import TestComponent from '../components/TestComponent.vue'
 
-export default Vue.extend({})
+export default Vue.extend({ components: { TestComponent } })
 </script>
+
+<style lang="scss" scoped>
+@import 'assets/scss/app';
+
+.index-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  min-height: 100vh;
+  background-color: pink;
+}
+</style>
